3b2588ef1da2c7a1a1e8bdf1f254e454b815d71c
Alexander <pchp250g8@tutamail.com>
2026-02-23 16:21:29 +0300

    Created Project Task-4-3

---
+++ README.md
@@ -0,0 +1,7 @@
+RobotTask-4-3. Python Tutorials. Actor Robot. StepInDev Platform. GUI Application. 
+
+Robot-Pos-N1
+
+
+
+Robot-Pos-N2
---
+++ Task-4-3.py
@@ -0,0 +1,14 @@
+from robot import *
+
+while is_free_right() or is_free_left() or is_free_up():
+    while is_free_right():
+        move_right()
+    paint()
+    while is_free_down():
+        move_down()
+    paint()
+    while is_free_left():
+        move_left()
+    paint()
+    while is_free_up():
+        move_up()
